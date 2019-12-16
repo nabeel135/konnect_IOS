fcabf8855b11a1e0fe5363903a9a4c901b67bd05
M Nabeel Hussain <tb135qet13@gmail.com>
2019-12-16 10:15:22 +0500

    Initial Commit

---
+++ Conektr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conektr
+//
+//  Created by Mr. Nabeel on 12/16/19.
+//  Copyright © 2019 Mr. Nabeel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
